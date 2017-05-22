b25bee7896c5996fcd96f332d38f94648ad0a264
Balthair94 <archerbaltazar@gmail.com>
2017-05-22 01:31:49 -0500

    working on send the email'

--- app/src/main/java/baltamon/mx/sendanemail/MainActivity.java
+++ app/src/main/java/baltamon/mx/sendanemail/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewHolder = new MainActivityViewHolder(findViewById(android.R.id.content));
         setUpToolbar();
+        sendEmail();
     }
 
     public void setUpToolbar(){
@@ -28,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setTitle("Send an email");
         }
 
+    }
+
+    public void sendEmail(){
+        viewHolder.getButtonSendEmail().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("SendMailActivity", "Send Button Clicked.");
+
+                String fromEmail = viewHolder.getEditTextFrom().getText().toString();
+                String fromPassword = viewHolder.getEditTextPassword().getText().toString();
+                String toEmails = viewHolder.getEditTextTo().getText().toString();
+                List toEmailList = Arrays.asList(toEmails
+                        .split("\\s*,\\s*"));
+                Log.i("SendMailActivity", "To List: " + toEmailList);
+                String emailSubject = viewHolder.getEditTextSubject().getText().toString();
+                String emailBody = viewHolder.getEditTextBody().getText().toString();
+                new SendMailTask(MainActivity.this).execute(fromEmail,
+                        fromPassword, toEmailList, emailSubject, emailBody);
+            }
+        });
     }
 }
