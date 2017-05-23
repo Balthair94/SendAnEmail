21883dbdc6c4f2553f53acc982052624a4d76c06
Balthair94 <archerbaltazar@gmail.com>
2017-05-23 01:21:20 -0500

    It is possible to send an email with a load image from the phone

--- app/src/main/java/baltamon/mx/sendanemail/MainActivity.java
+++ app/src/main/java/baltamon/mx/sendanemail/MainActivity.java
@@ -1,7 +1,10 @@
 package baltamon.mx.sendanemail;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -14,9 +17,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOAD_IMAGE = 02;
+
     private MainActivityViewHolder viewHolder;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (isOnline())
             sendEmail();
+
+        loadLocalImage();
     }
 
     public void setUpToolbar(){
@@ -60,14 +70,51 @@ public class MainActivity extends AppCompatActivity {
                 String[] TO = {"galuz01@gmail.com"};
 
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
+                if (imageUri != null){
+                    intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                    intent.setType("image/*");
+                }
                 intent.setData(Uri.parse("mailto:")); // only email apps should handle this
                 intent.putExtra(Intent.EXTRA_EMAIL, TO);
                 intent.putExtra(Intent.EXTRA_SUBJECT, viewHolder.getEditTextSubject().getText().toString());
                 intent.putExtra(Intent.EXTRA_TEXT, viewHolder.getEditTextBody().getText().toString());
-                if (intent.resolveActivity(getPackageManager()) != null) {
+                if (intent.resolveActivity(getPackageManager()) != null)
                     startActivity(intent);
-                }
+                else
+                    showToast("No email provider detected");
             }
         });
+    }
+
+    public void loadLocalImage(){
+        viewHolder.getButtonLoadLocalImage().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, LOAD_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case LOAD_IMAGE:
+                if (resultCode == Activity.RESULT_OK){
+                    imageUri = data.getData();
+                    try {
+                        Bitmap myBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        viewHolder.getImageView().setImageBitmap(myBitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
+
+    public void showToast(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/baltamon/mx/sendanemail/MainActivityViewHolder.java
+++ app/src/main/java/baltamon/mx/sendanemail/MainActivityViewHolder.java
@@ -3,6 +3,7 @@ package baltamon.mx.sendanemail;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by Baltazar Rodriguez on 22/05/2017.
@@ -13,13 +14,26 @@ public class MainActivityViewHolder {
     private EditText editTextSubject;
     private EditText editTextBody;
 
+    private ImageView imageView;
+
+    private Button buttonLoadLocalImage;
     private Button buttonSendEmail;
 
     public MainActivityViewHolder(View view){
         editTextTo = (EditText) view.findViewById(R.id.et_to_email);
         editTextSubject = (EditText) view.findViewById(R.id.et_subject_email);
         editTextBody = (EditText) view.findViewById(R.id.et_body_email);
+        imageView = (ImageView) view.findViewById(R.id.iv_image);
+        buttonLoadLocalImage = (Button) view.findViewById(R.id.btn_load_local_image);
         buttonSendEmail = (Button) view.findViewById(R.id.btn_send_email);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public Button getButtonLoadLocalImage() {
+        return buttonLoadLocalImage;
     }
 
     public EditText getEditTextTo() {
